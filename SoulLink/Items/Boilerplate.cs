96f06219d984f47f272b810367dc79aff581063d
Adam Haertter <80988984+adamhaertter@users.noreply.github.com>
2025-02-27 16:08:40 -0500

    Initial Commit - building off another project bc I'm a bad programmer

---
+++ SoulLink/Items/Boilerplate.cs
@@ -0,0 +1,109 @@
+﻿using R2API;
+using RoR2;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using PrestigeItems.Util;
+using UnityEngine.AddressableAssets;
+
+namespace PrestigeItems.Items
+{
+    internal class Boilerplate
+    {
+        // We need our item definition to persist through our functions, and therefore make it a class field.
+        public static ItemDef myItemDef;
+
+        internal static void Init()
+        {
+            // First let's define our item
+            myItemDef = ScriptableObject.CreateInstance<ItemDef>();
+
+            // Language Tokens, explained there https://risk-of-thunder.github.io/R2Wiki/Mod-Creation/Assets/Localization/
+            myItemDef.name = "EXAMPLE_CLOAKONKILL_NAME";
+            myItemDef.nameToken = "EXAMPLE_CLOAKONKILL_NAME";
+            myItemDef.pickupToken = "EXAMPLE_CLOAKONKILL_PICKUP";
+            myItemDef.descriptionToken = "EXAMPLE_CLOAKONKILL_DESC";
+            myItemDef.loreToken = "EXAMPLE_CLOAKONKILL_LORE";
+
+            // The tier determines what rarity the item is:
+            // Tier1=white, Tier2=green, Tier3=red, Lunar=Lunar, Boss=yellow,
+            // and finally NoTier is generally used for helper items, like the tonic affliction
+#pragma warning disable Publicizer001 // Accessing a member that was not originally public. Here we ignore this warning because with how this example is setup we are forced to do this
+            myItemDef._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/Base/Common/Tier2Def.asset").WaitForCompletion();
+#pragma warning restore Publicizer001
+            // Instead of loading the itemtierdef directly, you can also do this like below as a workaround
+            // myItemDef.deprecatedTier = ItemTier.Tier2;
+
+            // You can create your own icons and prefabs through assetbundles, but to keep this boilerplate brief, we'll be using question marks.
+            myItemDef.pickupIconSprite = Addressables.LoadAssetAsync<Sprite>("RoR2/Base/Common/MiscIcons/texMysteryIcon.png").WaitForCompletion();
+            myItemDef.pickupModelPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mystery/PickupMystery.prefab").WaitForCompletion();
+
+            // Can remove determines
+            // if a shrine of order,
+            // or a printer can take this item,
+            // generally true, except for NoTier items.
+            myItemDef.canRemove = true;
+
+            // Hidden means that there will be no pickup notification,
+            // and it won't appear in the inventory at the top of the screen.
+            // This is useful for certain noTier helper items, such as the DrizzlePlayerHelper.
+            myItemDef.hidden = false;
+
+            // You can add your own display rules here,
+            // where the first argument passed are the default display rules:
+            // the ones used when no specific display rules for a character are found.
+            // For this example, we are omitting them,
+            // as they are quite a pain to set up without tools like https://thunderstore.io/package/KingEnderBrine/ItemDisplayPlacementHelper/
+            var displayRules = new ItemDisplayRuleDict(null);
+
+            // Then finally add it to R2API
+            ItemAPI.Add(new CustomItem(myItemDef, displayRules));
+
+            // But now we have defined an item, but it doesn't do anything yet. So we'll need to define that ourselves.
+            GlobalEventManager.onCharacterDeathGlobal += GlobalEventManager_onCharacterDeathGlobal;
+        }
+
+        private static void GlobalEventManager_onCharacterDeathGlobal(DamageReport report)
+        {
+            // If a character was killed by the world, we shouldn't do anything.
+            if (!report.attacker || !report.attackerBody)
+            {
+                return;
+            }
+
+            var attackerCharacterBody = report.attackerBody;
+
+            // We need an inventory to do check for our item
+            if (attackerCharacterBody.inventory)
+            {
+                // Store the amount of our item we have
+                var garbCount = attackerCharacterBody.inventory.GetItemCount(myItemDef.itemIndex);
+                if (garbCount > 0 &&
+                    // Roll for our 50% chance.
+                    RoR2.Util.CheckRoll(50, attackerCharacterBody.master))
+                {
+                    // Since we passed all checks, we now give our attacker the cloaked buff.
+                    // Note how we are scaling the buff duration depending on the number of the custom item in our inventory.
+                    attackerCharacterBody.AddTimedBuff(RoR2Content.Buffs.Cloak, 3 + garbCount);
+                }
+            }
+        }
+
+        // String definitions / key lookup
+        private static void AddTokens()
+        {
+            myItemDef.name = "EXAMPLE_CLOAKONKILL_NAME";
+            myItemDef.nameToken = "EXAMPLE_CLOAKONKILL_NAME";
+            myItemDef.pickupToken = "EXAMPLE_CLOAKONKILL_PICKUP";
+            myItemDef.descriptionToken = "EXAMPLE_CLOAKONKILL_DESC";
+            myItemDef.loreToken = "EXAMPLE_CLOAKONKILL_LORE";
+
+            LanguageAPI.Add("EXAMPLE_CLOAKONKILL_NAME", "Cuthroat's Garb");
+            LanguageAPI.Add("EXAMPLE_CLOAKONKILL_PICKUP", "chance to cloak on kill.");
+            LanguageAPI.Add("DEVCUBE_DESCRIPTION", "Whenever you <style=cIsDamage>kill an enemy</style>, you have a <style=cIsUtility>50%</style> chance to cloak for <style=cIsUtility>4s</style> <style=cStack>(+1s per stack)</style>.");
+
+            string lore = "Those who visit in the night are either praying for a favour, or preying on a neighbour.";
+            LanguageAPI.Add("DEVCUBE_LORE", lore);
+        }
+    }
+}
---
+++ SoulLink/SoulLink.cs
@@ -0,0 +1,68 @@
+using BepInEx;
+using PrestigeItems.Items;
+using R2API;
+using RoR2;
+using SoulLink.Util;
+using UnityEngine;
+
+namespace SoulLink
+{
+    // You don't need this if you're not using R2API in your plugin,
+    // it's just to tell BepInEx to initialize R2API before this plugin so it's safe to use R2API.
+    [BepInDependency(ItemAPI.PluginGUID)]
+    [BepInDependency(LanguageAPI.PluginGUID)]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+    public class SoulLink : BaseUnityPlugin
+    {
+        // The Plugin GUID should be a unique ID for this plugin,
+        // which is human readable (as it is used in places like the config).
+        // If we see this PluginGUID as it is on thunderstore,
+        // we will deprecate this mod.
+        // Change the PluginAuthor and the PluginName !
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "BlueB";
+        public const string PluginName = "SoulLinkEquipment";
+        public const string PluginVersion = "0.0.0";
+
+        public static PluginInfo SavedInfo { get; private set; }
+
+        // The Awake() method is run at the very start when the game is initialized.
+        public void Awake()
+        {
+            SavedInfo = Info;
+
+            Log.Init(Logger);
+            AssetUtil.Init();
+
+            Log.Debug($"Asset Bundle loaded from stream. (allegedly)");
+
+            // Initialize item classes
+            DevCube.Init();
+            Boilerplate.Init(); // Disabled for now
+
+        }
+
+        // The Update() method is run on every frame of the game.
+        private void Update()
+        {
+            
+            // These are debug controls. I'm disabling them during normal gameplay, but keeping so I can test.
+            DebugSpawnItem(Boilerplate.myItemDef, KeyCode.F1);
+            
+        }
+
+        private void DebugSpawnItem(ItemDef itemDef, KeyCode keyTrigger)
+        {
+            if (Input.GetKeyDown(keyTrigger))
+            {
+                // Get the player body to use a position:
+                var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
+
+                // And then drop our defined item in front of the player.
+                Log.Info($"Player pressed {keyTrigger}. Spawning our custom item {itemDef.name} at coordinates {transform.position}");
+                PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(itemDef.itemIndex), transform.position, transform.forward * 20f);
+            }
+        }
+    }
+}
